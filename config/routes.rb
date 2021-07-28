7cb49bad20f762d49c820f12896c5585832d8a11
yukikosensei <wwwdtad@gmail.com>
2021-07-28 18:06:02 -0400

    Add locale

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  post "emails", to: "emails#create", as: "create_email"
-  get "emails/new", to: "emails#new", as: "new_email"
-  root "pages#index"
-  get "/japanese", to: "pages#japanese"
-  get "/studyrecord", to: "pages#studyrecord"
-
+  scope '(:locale)' do
+    post 'emails', to: 'emails#create', as: 'create_email'
+    get 'emails/new', to: 'emails#new', as: 'new_email'
+    root 'pages#index'
+    get '/japanese', to: 'pages#japanese'
+    get '/studyrecord', to: 'pages#studyrecord'
+  end
 end
